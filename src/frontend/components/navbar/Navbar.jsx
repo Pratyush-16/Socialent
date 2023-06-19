3aaaf93fe191cdfbfed776533cd9b5940038246d
Pratyush <76037546+Pratyush-16@users.noreply.github.com>
2023-06-19 23:59:31 +0530

    Feat: added pages

--- src/frontend/components/navbar/Navbar.jsx
+++ src/frontend/components/navbar/Navbar.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 
-export default function Navbar() {
+export const  Navbar =() => {
   return (
     <div>
+
+        <h1>My Socials</h1>
       
     </div>
   )
--- src/frontend/pages/Home/Home.jsx
+++ src/frontend/pages/Home/Home.jsx
@@ -1,11 +1,27 @@
 import React from 'react'
+import { Navbar } from '../../components/Navbar/Navbar'
+
 
 export default function Home() {
 
 
   return (
     <div>
-      <h1>Home page</h1>
+        <div>
+        <Navbar/>
+        </div>
+
+        <div>
+          <h1>Options</h1>
+        </div>
+        
+      <div>
+        <h1>HomeFeed</h1>
+      </div>
+
+      <div>
+        <h1>Suggestion Followers</h1>
+      </div>
     </div>
   )
 }
