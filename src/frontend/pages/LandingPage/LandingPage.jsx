55ff5da850592644c62e813f39bea74853a3c0b4
Pratyush <76037546+Pratyush-16@users.noreply.github.com>
2023-07-06 16:20:13 +0530

    Feat: styling landing page

--- src/frontend/pages/LandingPage/LandingPage.jsx
+++ src/frontend/pages/LandingPage/LandingPage.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import "./LandingPage.css"
 import { useNavigate } from 'react-router'
 
 export default function LandingPage() {
@@ -8,15 +9,40 @@ export default function LandingPage() {
 
 
   return (
-    <div>
+    <div className='main-landing-container'>
 
-        <h1>Welcome to MY Socials</h1>
+      <div className='heading'>
+      <h1 className='home-title'> MY Socials</h1>
+     
 
-        <div>
-            <button onClick={()=> navigate('/signup')}>Join us</button>
-            <p onClick={()=> navigate('/login')}>ALready have an account ?  Sign In</p>
+
+      <h2 class="sub-heading">
+            <span className='main-sub-heading'>FOLLOW</span><span className='suffix-title'>PEOPLE AROUND THE GLOBE</span>
+          </h2>
+          <h2 >
+            <span className='main-sub-heading'>CONNECT</span> <span className='suffix-title'>WITH YOUR FRIENDS</span>
+          </h2>
+          <h2  >
+            <span className='main-sub-heading'>SHARE</span> <span className='suffix-title'>WHAT YOU THINKING</span>
+          </h2>
+
+          <div className='button-container'>
+            <button className='button1' onClick={()=> navigate('/signup')}>Join us</button>
+            <p className='button2' onClick={()=> navigate('/login')}>ALready have an account ?  Sign In</p>
+        </div>
+
+          </div>
+        
+        
+
+      
+
+        <div className='image-container'>
+          <img src="https://i.postimg.cc/wTZ41mnQ/social-media.png" alt="" />
         </div>
       
     </div>
+
   )
+      
 }
